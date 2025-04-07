d49d9d4c35eb2c17d8422c1014fdff44a8ebb683
Daniel Krzykawski <daniel.krzykawski.01@gmail.com>
2025-04-07 21:02:42 +0200

    Make the app run with new dependencies
    - Add security.crypto and biometric dependencies
    - Change the SDK version to 35

--- app/src/main/java/com/example/securenoteapp/SecureDataActivity.kt
+++ app/src/main/java/com/example/securenoteapp/SecureDataActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.securenoteapp.ui.theme.SecureNoteAppTheme
 
-class MainActivity : ComponentActivity() {
+class SecureDataActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "SecureNoteApp"
 include(":app")
- 
